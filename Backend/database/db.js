b70faacfbc8a11ba49974b252984fd1869891660
Alaa <alaa.assali1996@outlook.com>
2020-02-10 19:03:42 +0100

    commit

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/meanauthdb'
+    db: 'mongodb://The50cent:02052007@cluster0-shard-00-00-ynd2e.mongodb.net:27017,cluster0-shard-00-01-ynd2e.mongodb.net:27017,cluster0-shard-00-02-ynd2e.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
